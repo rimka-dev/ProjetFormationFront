9488aa6f6cf2c2beff6a1e51faf059004806fb85
rimka-dev <k.takka01@gmail.com>
2022-05-10 21:48:40 +0200

    button connexion ok

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,7 +12,7 @@ import { ConectstagiaireService } from '../services/connexion/conectstagiaire.se
 export class HeaderComponent implements OnInit {
   requestFormation : Subscription | undefined;
   stagiaire : any;
-  isConnected : number = 0;
+  isConnected = true;
   constructor(private apiconnexionstagiaire : ConectstagiaireService,private route: Router) {}
 
   ngOnInit(): void {
@@ -23,12 +23,13 @@ export class HeaderComponent implements OnInit {
        
        
        this.stagiaire = result;
+       console.log("this.stagiaire = "+this.stagiaire);
       if(this.stagiaire == null){
-        this.isConnected = 0;
+        this.isConnected = false;
       }else{
-        this.isConnected = 1;
+        this.isConnected = true;
       }
-      console.log(this.isConnected);
+      console.log("this.isConnected = "+this.isConnected);
       
           console.log("connected :"+this.stagiaire.statut);
       
@@ -44,6 +45,7 @@ export class HeaderComponent implements OnInit {
     this.apiconnexionstagiaire.getDeconnexionStagiaires().subscribe({
       next: (result: any) => {}
   });
+  this.isConnected = false;
   location.reload();
   }
 
